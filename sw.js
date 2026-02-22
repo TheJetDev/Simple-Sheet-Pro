a35d6e2bd4fb31d42e4efc85352dcd904633ee50
TheJetDev <thejetdev.contact@gmail.com>
2026-02-22 16:21:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (PRO Version)
-const CACHE_NAME = 'simple-sheet-pro-v2.7.3'; // 
+const CACHE_NAME = 'simple-sheet-pro-v2.7.4'; // 
 
 const urlsToCache = [
   'index.html',
@@ -58,5 +58,6 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
 
 
